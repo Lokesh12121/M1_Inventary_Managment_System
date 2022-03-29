9a78977a1272467f5ce7279294d2e6fc808e347b
Lokesh <lokeshramu8441@gmail.com>
2022-03-29 15:54:32 +0530

    write .h file in inc

---
+++ 3_Implementation/inc/product_operation.h
@@ -0,0 +1,23 @@
+
+
+
+#ifndef __PHARMA_OPERATIONS_H__
+#define __PHARMA_OPERATIONS_H__
+
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+struct Medicine
+{
+   int id,price,quantity;
+   char medicneName[100],Company[100],Mfg_Date[11],Exp_Date[11];
+}m[100];
+
+void DisplayMedicine(int number);
+void SearchMedicine(int number);
+void AddMedicine(int number,struct Medicine m[]);
+void DeleteMedicine(int number);
+void UpdateMedicine(int number);
+
+#endif  
