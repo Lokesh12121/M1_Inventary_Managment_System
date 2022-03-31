e75bef94b9ecb5b0caa04dd4d490bf64d72ace90
Lokesh R <lokeshramu8441@gmail.com>
2022-03-31 18:49:57 +0530

    Update product_operations.h

--- 3_Implementation/inc/product_operations.h
+++ 3_Implementation/inc/product_operations.h
@@ -16,16 +16,44 @@
 #include<string.h>
 #include<ctype.h>
 
+/**
+* Create structure and features
+* @brief Creating structure named Prod 
+* @param[in] ID
+* @param[in] Price
+* @param[in] Quantity
+* @return Stores value in variables
+*/
+
 struct Prod
 {
    int id,price,quantity;
    char productName[100],Company[100],Mfg_Date[11],Exp_Date[11];
 }m[100];
 
+/**
+ * @brief Displays all products/items in Inventory 
+ */
 void DisplayProduct(int number);
+
+/**
+ * @brief Searches for an Item/Product in Inventory 
+ */
 void SearchProduct(int number);
+
+/**
+ * @brief Adds an item in Inventory
+ */
 void AddProduct(int number,struct Prod m[]);
+
+/**
+ * @brief Deletes an item in Inventory 
+ */
 void DeleteProduct(int number);
+
+/**
+ * @brief Updates/Modify an item in Inventory 
+ */
 void UpdateProduct(int number);
 
 #endif  /* _PRODUCT_OPERATIONS_H_ */
